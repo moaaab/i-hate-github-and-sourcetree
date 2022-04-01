a8a95135503d022f352ab31b8c79f95cdcfd9731
CM\moa.bergstrom <moa.bergstrom@futuregames.nu>
2022-04-01 15:30:13 +0200

    restart

--- Assets/Scripts/Danger.cs
+++ Assets/Scripts/Danger.cs
@@ -4,15 +4,9 @@ using UnityEngine;
 
 public class Danger : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public IsDead isDead;
+    private void OnCollisionEnter(Collision collision)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+         isDead.died = true;
     }
 }
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private float deathMenuAppearTime = 1f;
+
+    public Transform DeathMenuCanvas;
+    public IsDead isDead;
+
+
+    void Update()
+    {
+        if (isDead.died == true)
+        {
+            StartCoroutine(DeathMenu());
+        }
+    }
+
+    IEnumerator DeathMenu()
+    {
+        yield return new WaitForSeconds(deathMenuAppearTime);
+        DeathMenuCanvas.gameObject.SetActive(true);
+    }
+}

480bb11ae94164cb9ead791eb3712f7c2a8868c2
Simon Lindström <schismatron1@gmail.com>
2022-04-04 10:15:19 +0200

    playercontroller and camera mover (must use both for now)

---
+++ Assets/scripts/player.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    [SerializeField] public float playerGravity = 30;
+    [SerializeField] public float playerFVelocity = 0.1f; //forward velocity
+    [SerializeField] public bool playerGravityState = true; //true = gravity normal (falls down) - false = gravity flipped (falls up)
+    public Rigidbody rb;
+    //new Vector3
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Jump
+        /*if (Input.GetKeyDown("space"))
+        {
+            Debug.Log("space was pressed");
+            rb.velocity = new Vector3.up * playerJumpHeight;
+            //playerUpVelocity = playerJumpHeight;
+            playerGravityState = true;
+        }
+
+
+        //Gravity
+        if (playerGravityState == true)
+        {
+            playerUpVelocity -= playerGravityValue;
+        }
+        if (playerGravityState == false)
+        {
+            playerUpVelocity += playerGravityValue;
+        }
+        gameObject.transform.position += Vector3.down * (-playerUpVelocity);*/
+
+        //jump
+        if (Input.GetKeyDown("space"))
+        {
+            Debug.Log("space was pressed");
+            rb.velocity = new Vector3(0, 20, 0);
+            playerGravityState = true;
+        }
+        //reverse jump
+        if (Input.GetKeyDown("down"))
+        {
+            Debug.Log("down was pressed");
+            rb.velocity = new Vector3(0, -20, 0);
+            playerGravityState = false;
+        }
+        //Automatic movement forward
+        gameObject.transform.position += Vector3.forward * playerFVelocity;
+    }
+
+    private void FixedUpdate()
+    {
+        if (playerGravityState) {
+            Physics.gravity = new Vector3(0, -playerGravity, 0);
+        }
+        
+        if (!playerGravityState) {
+            Physics.gravity = new Vector3(0, playerGravity, 0);
+        }
+    }
+}

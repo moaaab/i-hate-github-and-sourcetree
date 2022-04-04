824230aee9425fe2b045bfad144540efb2539b7c
Simon Lindström <schismatron1@gmail.com>
2022-04-04 17:29:10 +0200

    test pls dont fuck things up

--- Assets/scripts/scrip.cs
+++ Assets/scripts/scrip.cs
@@ -4,11 +4,7 @@ using UnityEngine;
 
 public class scrip : MonoBehaviour
 {
-<<<<<<< HEAD
     [SerializeField] public float cameraSpeed = 10;
-=======
-    public float cameraSpeed = 0.1f;
->>>>>>> design_choices
     // Start is called before the first frame update
     void Start()
     {

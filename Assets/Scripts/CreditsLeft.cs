9a5e9bf3b11fd518044f4294c771207f72f447e6
CM\moa.bergstrom <moa.bergstrom@futuregames.nu>
2022-04-07 11:48:51 +0200

    Score when you die. very ugly

--- Assets/Scripts/CreditsLeft.cs
+++ Assets/Scripts/CreditsLeft.cs
@@ -7,9 +7,12 @@ public class CreditsLeft : MonoBehaviour
 {
     public Lives lives;
     public TextMeshProUGUI credits;
+    public TextMeshProUGUI scoreText;
+    public Score score;
 
     private void Update()
     {
         credits.text = lives.nrOfLives.ToString();
+        scoreText.text = score.meterScore.ToString(); 
     }
 }
